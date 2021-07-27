bde8267981feeeec2180c84828d25b154ec8ba8a
Sairion350 <76807368+Sairion350@users.noreply.github.com>
2021-07-27 05:18:25 -0500

    update script

--- OOPapDownload.py
+++ OOPapDownload.py
@@ -1,2 +1,2 @@
-import urllib
-urllib.urlretrieve("https://nightly.link/Sairion350/OOPapyrus/workflows/main/main/OOPapyrus.zip", "OOPapyrus.zip")
+from urllib.request import urlretrieve
+urlretrieve("https://nightly.link/Sairion350/OOPapyrus/workflows/main/main/OOPapyrus.zip", "OOPapyrus.zip")
