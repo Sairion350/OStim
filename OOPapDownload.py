de4d02f7cf503dceb9e92a2614631ec611800fc7
Sairion350 <76807368+Sairion350@users.noreply.github.com>
2021-07-27 05:32:28 -0500

    Script fix

--- OOPapDownload.py
+++ OOPapDownload.py
@@ -3,6 +3,6 @@ import zipfile
 
 
 urlretrieve("https://nightly.link/Sairion350/OOPapyrus/workflows/main/main/OOPapyrus.zip", "OOPapyrus.zip")
-zip = ZipFile('file.zip')
+zip = ZipFile('OOPapyrus.zip')
 zip.extractall()
 zip.close()
