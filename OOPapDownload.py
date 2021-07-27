9920f48f53084089240745cee77550d2938e2cb0
Sairion350 <76807368+Sairion350@users.noreply.github.com>
2021-07-27 05:30:44 -0500

    Python unzip

--- OOPapDownload.py
+++ OOPapDownload.py
@@ -1,2 +1,8 @@
 from urllib.request import urlretrieve
+import zipfile
+
+
 urlretrieve("https://nightly.link/Sairion350/OOPapyrus/workflows/main/main/OOPapyrus.zip", "OOPapyrus.zip")
+zip = ZipFile('file.zip')
+zip.extractall()
+zip.close()
